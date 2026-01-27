20833bd2dccc5518eb4801fe2845fbff50c48331
Yazid <yazasykurillah@gmail.com>
2026-01-27 07:05:47 +0700

    implement swiper.js to show project page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,33 @@
 import './bootstrap';
+import Swiper from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (document.querySelector('.project-swiper')) {
+        const swiper = new Swiper('.project-swiper', {
+            modules: [Navigation, Pagination, Autoplay],
+            loop: true,
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'fraction',
+                renderFraction: function (currentClass, totalClass) {
+                    return '<span class="' + currentClass + ' font-heading font-black text-2xl"></span>' +
+                        '<span class="font-heading font-black text-2xl mx-1">/</span>' +
+                        '<span class="' + totalClass + ' font-heading font-black text-2xl"></span>';
+                }
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            spaceBetween: 30,
+        });
+    }
+});
--- resources/views/portfolio/show.blade.php
+++ resources/views/portfolio/show.blade.php
@@ -10,12 +10,17 @@
         <div class="grid grid-cols-1 lg:grid-cols-12 gap-12">
             <div class="lg:col-span-8 space-y-8">
                 @if($project->images && count($project->images) > 0)
-                    <div class="space-y-8">
-                        @foreach($project->images as $image)
-                            <div class="border-4 border-black shadow-neo-lg bg-black">
-                                <img class="w-full h-auto block" src="{{ asset('storage/' . $image) }}" alt="{{ $project->title }}">
-                            </div>
-                        @endforeach
+                    <div class="swiper project-swiper">
+                        <div class="swiper-wrapper">
+                            @foreach($project->images as $image)
+                                <div class="swiper-slide">
+                                    <img class="w-full h-auto block" src="{{ asset('storage/' . $image) }}" alt="{{ $project->title }}">
+                                </div>
+                            @endforeach
+                        </div>
+                        <div class="swiper-button-next"></div>
+                        <div class="swiper-button-prev"></div>
+                        <div class="swiper-pagination"></div>
                     </div>
                 @endif
                 
